8ff2c2d14b45e6afd7dfbd7091e29895c1ba9b7e
iams3cond <andr3whunter@gmail.com>
2016-04-14 18:30:28 -0400

    Start of Part 2?

--- Configuration.h
+++ Configuration.h
@@ -7,18 +7,26 @@
 //
 
 
-
-
 class Configuration
 {
 public:
     Configuration(int rr, int cc);
+    Configuration(string fileName);
     bool placeTileAt(int rStart, int cStart, bool isHorizontal, int tileLength);
     void forbid(int r, int c);
     void dumpToScreen( ) const;
+    list<Placement> tilesOnBoard;
+    list<Placement> getPossiblePlacements(int tileLength);
+    
     
 private:
     int rows, cols;
     Square **board;
     int countOfTiles;
+};
+
+Class Placement
+{
+public:
+    
 };
